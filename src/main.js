c000b3efec5c97d25474277d951a983a39e9d928
Takuya87 <87933851+Takuya87@users.noreply.github.com>
2021-10-11 23:26:55 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ Vue.config.productionTip = false
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDvsa0roXa64eHyjOwWMWwhDAO8intJ5G4",
-    authDomain: "my-address-p-7d43e.firebaseapp.com",
-    projectId: "my-address-p-7d43e",
-    storageBucket: "my-address-p-7d43e.appspot.com",
-    messagingSenderId: "686326609408",
-    appId: "1:686326609408:web:142ef0322836ae23f74ae5"
+    apiKey: "×××××××××××××××××××××",
+    authDomain: "×××××××××××××××××××××",
+    projectId: "×××××××××××××××××××××",
+    storageBucket: "×××××××××××××××××××××",
+    messagingSenderId: "×××××××××××××××××××××",
+    appId: "×××××××××××××××××××××"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
